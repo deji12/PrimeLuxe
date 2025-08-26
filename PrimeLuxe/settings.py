074c369787c4719de4fd297049be18b7ae8c4786
deji12 <adesolaayodeji53@gmail.com>
2025-08-26 15:06:37 +0100

    deployment to server

--- PrimeLuxe/settings.py
+++ PrimeLuxe/settings.py
@@ -84,13 +84,24 @@ WSGI_APPLICATION = 'PrimeLuxe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql', 
+            'NAME': config('DB_NAME'),              
+            'USER': config('DB_USER'),         
+            'PASSWORD': config('DB_USER_PASSWORD'),                
+            'HOST': config('DB_HOST'),                        
+            'PORT': config('DB_PORT'),                             
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
