249dc42e57d3b185e76f0b59d10b369c0d58d0d9
deji12 <adesolaayodeji53@gmail.com>
2025-08-26 17:20:54 +0100

    configured csrf

--- PrimeLuxe/settings.py
+++ PrimeLuxe/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='*').split(',')
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 
